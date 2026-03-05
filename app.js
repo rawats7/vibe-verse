0e07fa839c4e9dc02ad8fcdd095b5384b12c6f43
Rajendra singh <rawatrajendrasingh33185@gmail.com>
2026-03-05 14:03:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,539 @@
+const songs = [
+    {
+ title: "Aakhiyan Da Jadu",
+ artist: "Sarbjot Ramta",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/akhiyan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/aakhiyan.jpg"
+},
+
+{
+ title: "Naina",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/naina.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/naina.jpg"
+},
+
+{
+ title: "Jogi",
+ artist: " thiarajxtt, Bir",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jogi.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jogi.jpg"
+},
+
+{
+ title: "Naadaniyan",
+ artist: "Akshath Acharya",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/naadaniyan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/naadaniyan.jpg"
+},
+
+{
+ title: "Kabhi Kabhi Aditi",
+ artist: "Rashid Ali",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/kabhi.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/kabhi.jpg"
+},
+
+{
+ title: "Dil Nu",
+ artist: "AP Dhillon",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/dilnu.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/dilnu.jpg"
+},
+
+{
+ title: "Thinking of You",
+ artist: "Katy Perry",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/thinking.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/thinking.jpg"
+},
+
+{
+ title: "By My Side",
+ artist: "AP Dhillon",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bymyside.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bymyside.jpg"
+},
+
+{
+ title: "Us",
+ artist: "Aric, Andaaz & Talha Ali",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/us.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/us.jpg"
+},
+
+{
+ title: "Sajjan Raji",
+ artist: "Satinder Sartaaj",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sajjan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sajjan.jpg"
+},
+
+{
+ title: "Aarzu",
+ artist: "Noor,Khan,Madhurxo",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/aarzu.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/aarzu.jpg"
+},
+
+{
+ title: "Jind Mahi",
+ artist: "Harneet Singh Sethi (Ft. Johal MuSick)",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jindmahi.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jindmahi.jpg"
+},
+
+{
+ title: "Saibo",
+ artist: "Shreya Ghoshal & Tochi Raina",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/saibo.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/saibo.jpg"
+},
+
+{
+ title: "Sooiyan Sooiyan",
+ artist: "Arijit Singh & Chinmayi",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sooiyan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sooiyan.jpg"
+},
+
+{
+ title: "9:45",
+ artist: "Prabh Singh & Jay Trak",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/945.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/945.jpg"
+},
+
+{
+ title: "Bargad",
+ artist: "Arpit Bala",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bargad.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bargad.jpg"
+},
+
+{
+ title: "Zulfein",
+ artist: " Mehul Mahesh & Dj AYnik",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/zulfein.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/zulfein.jpg"
+},
+
+{
+ title: "Vaaroon",
+ artist: "Romy",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/vaaroon.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/vaaroon.jpg"
+},
+
+{
+ title: "Ishqaa Ve",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishqaa.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishqaa.jpg"
+},
+
+{
+ title: "Chaand Baaliyan",
+ artist: "Aditya A",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/chaand.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/chaand.jpg"
+},
+
+{
+ title: "Ambarsariya",
+ artist: "Sona Mohapatra",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ambarsariya.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ambarsariya.jpg"
+},
+{
+ title: "Tere Bina",
+ artist: "A.R. Rahman, Chinmayi",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/tere_bina.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/tere_bina.jpg"
+},
+
+{
+ title: "Aaj Se Teri",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/aaj_se_teri.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/aaj_se_teri.jpg"
+},
+
+
+{
+ title: "Paaro",
+ artist: "Aditya Rikhari",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/paaro.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/paaro.jpg"
+},
+
+{
+ title: "Arz Kiya Hai",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/arz_kiya_hai.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/arz_kiya_hai.jpg"
+},
+{
+ title: "Atrangi Re",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/atrangi_re.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/atrangi_re.jpg"
+},
+
+{
+ title: "Bulleya",
+ artist: "Amit Mishra & Shilpa Rao",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bulleya.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bulleya.jpg"
+},
+{
+ title: "Chann Sitare (Chavan Soneya)",
+ artist: "Ammy Virk",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/chavan_soneya.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/chavan_soneya.jpg"
+},
+{
+ title: "Channa Ve",
+ artist: "Akhil Sachdeva & Mansheel Gujral",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/channa_ve.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/channa_ve.jpg"
+},
+
+{
+ title: "Haath Tu Deja",
+ artist: "Himachali Folk",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/haath_tu_deja.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/haath_tu_deja.jpg"
+},
+{
+ title: "Hey Meri Pyari",
+ artist: "Aditya Rikhari",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/hey_meri_pyari.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/hey_meri_pyari.jpg"
+},
+{
+ title: "Hosanna",
+ artist: "A.R. Rahman, Vijay Prakash & Suzanne D'Mello",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/hosanna.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/hosanna.jpg"
+},
+
+{
+ title: "Ishq Di Baajiyan",
+ artist: "Diljit Dosanjh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishq_di_baajiyan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishq_di_baajiyan.jpg"
+},
+{
+ title: "Ishq Sufiyana",
+ artist: "Kamal Khan",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishq_sufiyana.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ishq_sufiyana.jpg"
+},
+
+{
+ title: "Jalsa",
+ artist: "Satinder Sartaaj",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jalsa.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jalsa.jpg"
+},
+{
+ title: "Kali Kali Zulfon Ke",
+ artist: "Nusrat Fateh Ali Khan",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/kali_kali_zulfon_ke.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/kali_kali_zulfon_ke.jpg"
+},
+
+{
+ title: "Laavan",
+ artist: "Jasmine Sandlas",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/laavan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/laavan.jpg"
+},
+
+{
+ title: "Khudaya Khair",
+ artist: "Billu Barber (Monali Thakur & Soham)",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/khudaya_khair.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/khudaya_khair.jpg"
+},
+{
+ title: "Main Ghat Banaras Ka",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/main_ghat_banaras_ka.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/main_ghat_banaras_ka.jpg"
+},
+
+{
+ title: "Wishes",
+ artist: "Hasan Raheem",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/wishes.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/wishes.jpg"
+},
+
+
+{
+ title: "Qaafirana",
+ artist: "Arijit Singh & Nikhita Gandhi",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/qaafirana.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/qaafirana.jpg"
+},
+{
+ title: "Ratiyaan",
+ artist: "Hanshika ",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ratiyaan.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/ratiyaan.jpg"
+},
+
+{
+ title: "Vanjhali Vaja",
+ artist: "Amrinder Gill",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/vanjhali_vaja.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/vanjhali_vaja.jpg"
+},
+{
+ title: "Sohna Phul",
+ artist: "Harsh Nussi & Deep Khair",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sohna_phul.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/sohna_phul.jpg"
+},
+{
+ title: "The Last Letter",
+ artist: "Maan Pannu",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/the_last_letter.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/the_last_letter.jpg"
+},
+{
+ title: "Dil Tu Jaan Tu",
+ artist: "Gurnazar",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/dil_tu_jaan_tu.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/dil_tu_jaan_tu.jpg"
+},
+
+    
+{
+ title: "Main Tera Ho Gaya",
+ artist: "Arijit Singh",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/main_tera_ho_gaya.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/main_tera_ho_gaya.jpg"
+},
+{
+ title: "Mera Yaar",
+ artist: "Shankar-Ehsaan-Loy",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/mera_yaar.mp3",
+ cover: "mera_yaar.jpg"
+},
+{
+ title: "With You",
+ artist: "AP Dhillon",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/with_you.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/with_you.jpg"
+},
+{
+ title: "Do Pall",
+ artist: "Coach Sahb",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/do_pall.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/do_pall.jpg"
+},
+{
+ title: "LONG TIME NO SEE",
+ artist: "taimour baig",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/long_time_no_see.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/long_time_no_see.jpg"
+},
+{
+ title: "HUSN",
+ artist: "Anuv Jain ",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/husn.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/husn.jpg"
+},
+{
+ title: "JHOL",
+ artist: "manu,annural khalid",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jhol.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/jhol.jpg"
+},
+{
+ title: "Bardali",
+ artist: "Sushant KC ",
+ file: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bardali.mp3",
+ cover: "https://raw.githubusercontent.com/rawats7/vibeverse/main/bardali.jpg"
+},
+
+];
+
+const playlist = document.getElementById("playlist");
+const audio = document.getElementById("audio");
+const progressBar = document.getElementById("progressBar");
+const volume = document.getElementById("volume");
+const timeDisplay = document.getElementById("time");
+const searchInput = document.getElementById("searchInput");
+const notFoundMessage = document.getElementById("notFoundMessage");
+
+const playPauseBtn = document.getElementById("playPauseBtn");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentIndex = 0;
+let isPlaying = false;
+
+// ---------- Render Playlist ----------
+function renderSongs(songList) {
+    playlist.innerHTML = "";
+    songList.forEach((song, idx) => {
+        const div = document.createElement("div");
+        div.className = "song";
+        div.innerHTML = `
+            <div style="display:flex;align-items:center;flex:1;">
+                <img src="${song.cover}">
+                <div class="song-info">
+                    <div class="song-title">${song.title}</div>
+                    <div class="song-artist">${song.artist}</div>
+                </div>
+            </div>
+            <button class="download-btn">⋮
+                <div class="download-options">
+                    <a href="${song.file}" download>
+                        ⬇ Download
+                    </a>
+                </div>
+            </button>
+        `;
+        // Song click
+        div.querySelector(".song-info").parentElement.onclick = (e) => {
+            if (!e.target.classList.contains("download-btn")) {
+                selectSong(idx);
+            }
+        };
+
+        
+        // Download button hover toggle
+        const downloadBtn = div.querySelector(".download-btn");
+        const options = div.querySelector(".download-options");
+        downloadBtn.addEventListener("click", (e) => {
+            e.stopPropagation();
+            options.style.display = options.style.display === "block" ? "none" : "block";
+        });
+        playlist.appendChild(div);
+    });
+}
+
+// ---------- Load & Play ----------
+function loadSong() {
+    const song = songs[currentIndex];
+    audio.src = song.file;
+    document.getElementById("title").innerText = song.title;
+    document.getElementById("artist").innerText = song.artist;
+    document.getElementById("cover").src = song.cover;
+
+    // Highlight active song
+    document.querySelectorAll(".song").forEach((s, idx) => {
+        s.classList.toggle("active", idx === currentIndex);
+    });
+}
+
+function playSong() {
+    audio.play();
+    isPlaying = true;
+    playPauseBtn.innerText = "⏸";
+}
+
+function pauseSong() {
+    audio.pause();
+    isPlaying = false;
+    playPauseBtn.innerText = "▶";
+}
+
+// ---------- Toggle Play/Pause ----------
+playPauseBtn.addEventListener("click", () => {
+    if (isPlaying) pauseSong();
+    else playSong();
+});
+
+// ---------- Previous / Next ----------
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + songs.length) % songs.length;
+    loadSong();
+    playSong();
+});
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % songs.length;
+    loadSong();
+    playSong();
+});
+
+// ---------- Select Song ----------
+function selectSong(idx) {
+    currentIndex = idx;
+    loadSong();
+    playSong();
+}
+
+// ---------- Progress Bar ----------
+audio.addEventListener("timeupdate", () => {
+    if (audio.duration) {
+        const percent = (audio.currentTime / audio.duration) * 100;
+        progressBar.style.width = percent + "%";
+        const min = Math.floor(audio.currentTime / 60);
+        const sec = Math.floor(audio.currentTime % 60).toString().padStart(2, "0");
+        const durMin = Math.floor(audio.duration / 60);
+        const durSec = Math.floor(audio.duration % 60).toString().padStart(2, "0");
+        timeDisplay.innerText = `${min}:${sec} / ${durMin}:${durSec}`;
+    }
+});
+
+function seek(e) {
+    const width = e.currentTarget.clientWidth;
+    audio.currentTime = (e.offsetX / width) * audio.duration;
+}
+
+// ---------- Volume ----------
+volume.addEventListener("input", () => (audio.volume = volume.value));
+
+// ---------- Autoplay ----------
+window.addEventListener("load", () => {
+    loadSong();
+    playSong();
+});
+
+// ---------- Stop audio when user interacts elsewhere ----------
+document.body.addEventListener("click", (e) => {
+    if (!e.target.closest(".player") && !e.target.closest(".song")) {
+        pauseSong();
+    }
+});
+
+// ---------- Search Functionality ----------
+searchInput.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
+    const filtered = songs.filter(
+        (song) =>
+            song.title.toLowerCase().includes(value) ||
+            song.artist.toLowerCase().includes(value)
+    );
+
+    if (filtered.length === 0) {
+        playlist.innerHTML = "";
+        notFoundMessage.style.display = "block";
+    } else {
+        notFoundMessage.style.display = "none";
+        renderSongs(filtered);
+    }
+});
+
+// ---------- Initial Render ----------
+renderSongs(songs);
+
+// ---------- Next Song Function ----------
+function nextSong() {
+    currentIndex = (currentIndex + 1) % songs.length; // loop to first after last
+    loadSong();
+    audio.play(); // directly play next song
+    isPlaying = true;
+    playPauseBtn.innerText = "⏸";
+}
+
+
+
+
